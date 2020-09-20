166378db152c43f22be6917b9da7607f3703b8b2
Will <williamjohnjames97@gmail.com>
2020-09-20 11:45:45 -0400

    adding btn

--- proD.php
+++ proD.php
@@ -1,11 +1,15 @@
 <?php include './inc/header.php' ?>
-<div class="contaienr">
+<div class="container">
     <div class="card">
         <div class="card-header text-center">
-            <h2>Are you sure you want to DELETE your account?</h2>
+            <h2><small>Are you sure you want to DELETE your account?</small></h2>
         </div>
         <div class="card-body">
+            <form action="">
 
+                <button type="submit" class="btn btn-outline-dark">YES</button>
+                <a href="./settings" class="btn btn-outline-primary" role="button">NO</a>
+            </form>
         </div>
     </div>
 
