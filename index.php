3988a0d5cff3135a4b80ed6008f3e0d253dffacd
Will <williamjohnjames97@gmail.com>
2020-09-29 16:45:33 -0400

    sc_3

--- index.php
+++ index.php
@@ -19,57 +19,62 @@ if (isset($_POST) & !empty($_POST)) {
 ?>
 </br>
 <div class="container">
-    <div id="demo" class="carousel slide" data-ride="carousel">
-        <ul class="carousel-indicators">
-            <li data-target="#demo" data-slide-to="0" class="active"></li>
-            <li data-target="#demo" data-slide-to="1"></li>
-            <li data-target="#demo" data-slide-to="2"></li>
-        </ul>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="./style/images/xps-computer.jpg" alt="XPS Computer" width="1100" height="500">
-                <div class="carousel-caption">
-                    <h3>Programming</h3>
-                    <p>Check out the latest news on programming?</p>
+    <div class="row">
+        <?php
+        if (!isset($_SESSOIN['useremail']) xor isset($_SESSION['username'])) {
+            echo '<div class="col-md-8">';
+        } else {
+            echo ' <div class="col-md-12">';
+        };
+        ?>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+            <ul class="carousel-indicators">
+                <li data-target="#demo" data-slide-to="0" class="active"></li>
+                <li data-target="#demo" data-slide-to="1"></li>
+                <li data-target="#demo" data-slide-to="2"></li>
+            </ul>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="./style/images/xps-computer.jpg" alt="XPS Computer" width="1100" height="500">
+                    <div class="carousel-caption">
+                        <h3>Programming</h3>
+                        <p>Check out the latest news on programming?</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./style/images/general.jpg" alt="type writer" width="1100" height="500">
+                    <div class="carousel-caption">
+                        <h3>General Talk</h3>
+                        <p>What are your friends talking about?</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./style/images/vgComputer.jpg" alt="New York" width="1100" height="500">
+                    <div class="carousel-caption">
+                        <h3>Gaming</h3>
+                        <p>What is happening in the ever expanding world of gaming?</p>
+                    </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="./style/images/general.jpg" alt="type writer" width="1100" height="500">
-                <div class="carousel-caption">
-                    <h3>General Talk</h3>
-                    <p>What are your friends talking about?</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="./style/images/vgComputer.jpg" alt="New York" width="1100" height="500">
-                <div class="carousel-caption">
-                    <h3>Gaming</h3>
-                    <p>What is happening in the ever expanding world of gaming?</p>
-                </div>
-            </div>
+            <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#demo" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#demo" data-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </a>
-        <a class="carousel-control-next" href="#demo" data-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </a>
     </div>
-
-</div>
-</br>
-<?php
-if (!isset($_SESSOIN['useremail']) xor isset($_SESSION['username'])) {
-    echo '
+    <div class="col-md-4">
+        <?php
+        if (!isset($_SESSOIN['useremail']) xor isset($_SESSION['username'])) {
+            echo '
     <div class="container">
     <div class="row">
-        <div class="col-md-3">
 
-        </div>
-        <div class="col-md-6">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                    <h2 class="card-title text-center">Please Sign in</h2>
+                    <h2 class="card-title text-center">Sign in</h2>
                     <form class="form" role="form" action="" method="POST">
                         <div class="form-group">
                             <label for="useremail">Email:</label>
@@ -87,15 +92,19 @@ if (!isset($_SESSOIN['useremail']) xor isset($_SESSION['username'])) {
                 </div>
             </div>
         </div>
-        <div class="col-md-3">
-
-        </div>
     </div>
 </div>
     ';
-} else {
-}
-?>
+        } else {
+        }
+        ?>
+    </div>
+</div>
+
+
+</div>
+</br>
+
 
 </br>
 <div class="container">
@@ -131,5 +140,6 @@ if (!isset($_SESSOIN['useremail']) xor isset($_SESSION['username'])) {
         </div>
     </div>
 </div>
+</br>
 
 <?php include './inc/footer.php' ?>
